0da20f498ac45b2caca9ed175995fa4fd8d490f0
Sonat Akça <sonatakcaa@gmail.com>
2025-10-28 19:26:24 +0000

    open as app

--- LocalWatch.bat
+++ LocalWatch.bat
@@ -1,5 +1,11 @@
+@REM @echo off
+@REM cd /d "%~dp0"
+@REM start cmd /k "npm run dev"
+@REM timeout /t 1 >nul
+@REM start "" "http://localhost:3000"
+
 @echo off
 cd /d "%~dp0"
-start cmd /k "npm run dev"
+start "" cmd /k "npm run dev"
 timeout /t 1 >nul
-start "" "http://localhost:3000"
+start "" "%~dp0LocalWatch.lnk"
